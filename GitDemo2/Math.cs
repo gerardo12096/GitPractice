50082aeaa1630a01e0e66dc8b841102e594a0a45
Gerardo Baron Diaz <gerardo.baron@hotmail.com>
2025-09-29 14:28:40 -0700

    Commit 1: Multiply checked

--- GitDemo2/Math.cs
+++ GitDemo2/Math.cs
@@ -10,7 +10,8 @@ namespace GitDemo2
     {
         public static int Add(int a, int b) => a + b;
         public static int Subtract(int a, int b) => a - b;
-        public static int Multiply(int a, int b) => a * b;
+        public static int Multiply(int a, int b) => checked(a * b);
+
         public static int Divide(int a, int b)
         {
             if(b <= 0) throw new DivideByZeroException("Denominator must be positive.");
